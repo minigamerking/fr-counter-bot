8cce046e47ac36c41aa45f25c07a21ff6bc8774f
MinGamerKing <136130810+minigamerking@users.noreply.github.com>
2024-02-28 18:13:08 +0000

    commit

--- frcount.py
+++ frcount.py
@@ -1,16 +1,48 @@
+import discord
+import sqlite3
+import os
+from flask import Flask, render_template
+from threading import Thread
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '''<body style="margin: 0; padding: 0;">
+    <iframe width="100%" height="100%" src="https://axocoder.vercel.app/" frameborder="0" allowfullscreen></iframe>
+  </body>'''
+
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+
+keep_alive()
+print("Server Running Because of Axo")
 # This code is based on the following example:
 # https://discordpy.readthedocs.io/en/stable/quickstart.html#a-minimal-bot
 
-import os
+conn = sqlite3.connect('fr_count.db')
+c = conn.cursor()
 
-import discord
+# Create the table if it doesn't exist
+c.execute('''CREATE TABLE IF NOT EXISTS fr_count
+             (guild_id INTEGER PRIMARY KEY, count INTEGER)''')
+conn.commit()
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-fr_count = 0
+fr_count = 8
+
 
 @client.event
 async def on_ready():
@@ -23,18 +55,22 @@ async def on_message(message):
     if message.author == client.user:
         return
     if 'fr' in message.content.lower():
-        fr_count += 1
-        print(f"FR Count: {fr_count}")
+        c.execute(
+            'UPDATE fr_count SET count = count + 1 WHERE guild_id = ?', (message.guild.id,))
+        conn.commit()
 
     if message.content.startswith('$counter'):
-        await message.channel.send(f'Amount of FRs: {fr_count}')
+        c.execute('SELECT count FROM fr_count WHERE guild_id = ?',
+                  (message.guild.id,))
+        count = c.fetchone()[0]
+        await message.channel.send(f'Amount of FRs: {count}')
 
 
 try:
-  token = os.getenv("TOKEN") or ""
-  if token == "":
-    raise Exception("Please add your token to the Secrets pane.")
-  client.run(token)
+    token = "MTIxMjQyNzU4OTQzNjcwMjcyMA.Gz-Oej.RwNFPp_ksJ6HMIZeNA0fZHHELDABqPTRFhUGZk"
+    if token == "":
+        raise Exception("Please add your token to the Secrets pane.")
+    client.run(token)
 except discord.HTTPException as e:
     if e.status == 429:
         print(
@@ -45,3 +81,4 @@ except discord.HTTPException as e:
         )
     else:
         raise e
+conn.close()
